2b95dcf58f0fbb23428a74da25fa9babeec94816
IrisQ <eloise_zyf@hotmail.com>
2017-03-23 23:22:46 -0400

    Create permutation_treepaths.java

---
+++ Searching_backtracking/permutation_treepaths.java
@@ -0,0 +1,16 @@
+/**
+ * 46. Permutations
+ * Given a collection of [distinct] numbers, return all possible permutations.
+ * eg: [1, 2, 3], get:[ [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
+ * 
+ * 257. Binary Tree Paths
+ * Given a binary tree, return all root-to-leaf paths. eg: ["1->2->5", "1->3"]
+ *
+ * [SOLUTION]: Both use dfs helper function to track path.
+ *             KEY [In what kind of conditions should return/add into result?]
+ * >>>> when need to pass unfinished string, use StringBuilder
+ */
+ public class Solution {
+    
+ }
+ 
