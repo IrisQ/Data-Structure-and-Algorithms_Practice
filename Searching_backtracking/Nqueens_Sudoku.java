504954acb1f85b6c185ab96639eebef41b4fc26c
IrisQ <eloise_zyf@hotmail.com>
2017-03-24 12:42:52 -0400

    Create Nqueens_Sudoku.java

---
+++ Searching_backtracking/Nqueens_Sudoku.java
@@ -0,0 +1,25 @@
+/**
+ * 51. N-Queens
+ * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+ * Given an integer n, return all distinct solutions to the n-queens puzzle.
+ * eg: 4 queens: 
+ * [ [".Q..",  // Solution 1
+ *    "...Q",
+ *    "Q...",
+ *    "..Q."],
+
+ *   ["..Q.",  // Solution 2
+ *    "Q...",
+ *    "...Q",
+ *    ".Q.."]]]
+ *
+ * 52. N-Queens II
+ * Return the total number of distinct solutions, instead of board configurations.
+ 
+ * 36. Valid Sudoku
+ * Determine if a Sudoku is valid.
+ 
+ * 37. Sudoku Solver
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+ * Empty cells are indicated by the character '.', assume only one unique solution.
+ */
