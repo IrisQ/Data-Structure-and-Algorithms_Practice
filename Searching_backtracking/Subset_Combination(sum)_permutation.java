2e97b2315d25551332b15212de07550910795b1e
IrisQ <eloise_zyf@hotmail.com>
2017-03-23 23:33:34 -0400

    Create Subset_Combination(sum)_permutation.java

---
+++ Searching_backtracking/Subset_Combination(sum)_permutation.java
@@ -0,0 +1,36 @@
+/**
+ * 1. Lintcode-Permutations
+ * Given a list of numbers, return all possible permutations. eg:[1, 2, 3]
+ 
+ * 2. Lintcode-Permutations II
+ * Given a list of numbers, return all possible permutations. eg: [1, 2, 2]
+ 
+ * 78. Subsets
+ * Given a set of [distinct] integers, nums, return all possible subsets.eg:[1, 2, 3]
+
+ * 90. Subsets II
+ * not distinct integers, [may contain duplicates], return all possible subsets.eg:[1, 2, 2]
+ 
+ * 77. Combinations
+ * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ 
+ * 39. Combination Sum
+ * Given a set of candidate numbers (C) (without duplicates) and a target number (T), 
+ * find all unique combinations in C where the candidate numbers sums to T.
+ * The same repeated number may be chosen from C unlimited number of times.
+ 
+ * 40. Combination Sum II
+ * Each number in C may only be used once in the combination.
+ 
+ * 216. Combination Sum III
+ * Find all possible combinations of k numbers that add up to a number n, given that only numbers 
+ * from 1 to 9 can be used and each combination should be a unique set of numbers.
+
+ * 377. Combination Sum IV
+ * Given an integer array with all positive numbers and no duplicates, find the number of 
+ * possible combinations that add up to a positive integer target
+ */
+ 
+ public class Solution {
+ 
+ }
