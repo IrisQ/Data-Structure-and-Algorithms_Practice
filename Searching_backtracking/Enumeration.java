140443116d29573372675cb26b41bd636c2cf846
IrisQ <eloise_zyf@hotmail.com>
2017-03-24 12:31:09 -0400

    Create Enumeration.java

---
+++ Searching_backtracking/Enumeration.java
@@ -0,0 +1,18 @@
+/**
+ * 22. Generate Parentheses
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * eg: n = 3, get ["((()))", "(()())",  "(())()", "()(())", "()()()"]
+  
+ * 93. Restore IP Addresses
+ * Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+ * Given "25525511135", return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+ 
+ * 131. Palindrome Partitioning
+ * Given a string s, partition s such that every substring of the partition is a palindrome.
+ * Return all possible palindrome partitioning of s. eg: s = "aab"
+ * [ ["aa","b"], ["a","a","b"]]
+ 
+ * 17. Letter Combinations of a Phone Number
+ * Given a digit string, return all possible letter combinations that the number could represent.
+ * Input:Digit string "23", Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+ */
