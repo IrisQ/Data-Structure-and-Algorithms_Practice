f6e84f564366e4ca62221c276406a8a92b545aca
IrisQ <eloise_zyf@hotmail.com>
2017-03-25 16:23:43 -0400

    Update Nqueens_Sudoku.java

--- Searching_backtracking/Nqueens_Sudoku.java
+++ Searching_backtracking/Nqueens_Sudoku.java
@@ -26,6 +26,81 @@
 
 public class Nqueens_Sudoku {
     /**
+     * 51. N-Queens
+     * [SOLUTION]: 1. queen is valid only if: no queen in same row/column/diagonal
+     *             2. use queens[n], queens[2] = 1: a queen (2, 1) -> for queens in each i, queens[i] should be unique
+     *                use dfs and loop through each to find match.
+     * Time: O(n!)
+     */
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        int[] queens = new int[n];
+        if (n <= 0) return res;
+        dfs(res, queens, n, 0);
+        return res;
+    }
+    // cur: the current row working on.
+    private void dfs(List<List<String>> res, int[] queens, int n, int cur) {
+        if (cur == n) {       // have already processed last row, now add into res.
+            List<String> list = new ArrayList<>();
+            for (int queen:queens) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0;j < queen;j++) {
+                    sb.append('.');
+                }
+                sb.append('Q');
+                for (int j = queen + 1;j < n;j++) {
+                    sb.append('.');
+                }
+                list.add(sb.toString());
+            }
+            res.add(list);
+        } else {
+            for (int i = 0;i < n;i++) {     // try each number to find a match
+                queens[cur] = i;
+                if (isValid(queens, cur)) {
+                    dfs(res, queens, n, cur + 1);
+                }
+            }
+        }
+    }
+    // if 2 queens in same 45' angle: x1 - x2 = y1 - y2.
+    // if 2 queens in same 135' angle: x1 + y1 = x2 + y2.
+    boolean isValid(int[] queens, int pos) {
+        for (int i = 0;i < pos;i++) {
+            // row is initially different, same column || 45' aggle || 135' angle
+            if (queens[i] == queens[pos] || queens[i] - queens[pos] == i - pos || i + queens[i] == pos + queens[pos]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * 52. N-Queens II -- Only need # of distinct solutions.
+     * [SOLUTION]: just need to update count.
+     */
+    public int totalNQueens(int n) {
+        if (n <= 0) return 0;
+        int[] queens = new int[n];
+        return dfs(queens, n, 0, 0);
+    }
+    
+    private int dfs(int[] queens, int n, int cur, int count) {
+        if (cur == n) {
+            return count + 1;
+        } else {
+            for (int i = 0;i < n;i++) {
+                queens[cur] = i;
+                if (isValid(queens, cur)) {
+                    count = dfs(queens, n, cur + 1, count);
+                }
+            }
+        }
+        return count;
+    }
+    
+    /**
      * [Solution]: Use 3 matrix to record what numbers have appear before in each ror/column/block.
      * Time: O(91)
      */
