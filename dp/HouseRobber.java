811bf6f84a975b00ea161e6b47b1d36abe75a76c
IrisQ <eloise_zyf@hotmail.com>
2017-03-24 20:43:22 -0400

    Create HouseRobber.java

---
+++ dp/HouseRobber.java
@@ -0,0 +1,11 @@
+/**
+ * 198. House Robber
+ * Given a non-negative integers array, get maximum amount of money without accessing two adjacent houses.
+ 
+ * 213. House Robber II
+ * All houses are arranged in a circle.
+ 
+ * 337. House Robber III
+ * all houses in this place forms a binary tree,  It will automatically contact the police if two directly-linked houses 
+ * were broken into on the same night. Determine the maximum amount of money can rob without alerting police.
+ */
