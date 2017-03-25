6c7c1f402239f0c68694777519e7f6bc864b10c5
IrisQ <eloise_zyf@hotmail.com>
2017-03-24 21:55:35 -0400

    Create PaintFence_House.java

---
+++ dp/PaintFence_House.java
@@ -0,0 +1,12 @@
+/**
+ * 276. Paint Fence
+ * A fence with n posts, each post can be painted with one of the k colors.
+ * HOW many ways are there to make sure <= 2 adjacent fence posts have the same color?
+ 
+ * 256. Paint House
+ * A row of n houses, each house can be painted red, blue or green, cost of painting each house with a certain color is different.
+ * Find minimum cost to paint all houses so that no two adjacent houses have the same color.
+ 
+ * 265. Paint House II
+ * Each house can be painted with one of the k colors.
+ */
