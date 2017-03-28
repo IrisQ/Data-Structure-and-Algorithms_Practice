67aa459c323733d9eada57a234732f04d1fd08d9
IrisQ <eloise_zyf@hotmail.com>
2017-03-28 19:50:14 -0400

    Update DFS_2D.java

--- Searching_backtracking/DFS_2D.java
+++ Searching_backtracking/DFS_2D.java
@@ -34,4 +34,31 @@ public class DFS_2D {
         return false;
     }
     
+    /**
+     * 200. Number of Islands 一遍过
+     */
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int m = grid.length, n = grid[0].length;
+        int count = 0;
+        for (int i = 0;i < m;i++) {
+            for (int j = 0;j < n;j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);    // set all '1' connected with this one to '0'
+                }
+            }
+        }
+        return count;
+    }
+    private void dfs(char[][] grid, int x, int y) {
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || 
+           grid[x][y] != '1') return;
+        grid[x][y] = '0';
+        dfs(grid, x - 1, y);
+        dfs(grid, x + 1, y);
+        dfs(grid, x, y - 1);
+        dfs(grid, x, y + 1);
+    }
+    
 }
