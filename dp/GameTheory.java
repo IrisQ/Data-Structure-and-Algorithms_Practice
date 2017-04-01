5043b57cf71ffccbf9ac6273b12aeba985b579b4
IrisQ <eloise_zyf@hotmail.com>
2017-03-31 23:14:30 -0400

    Update GameTheory.java

--- dp/GameTheory.java
+++ dp/GameTheory.java
@@ -24,5 +24,75 @@
  * MinMax Algorithm.
  */
 public class GameTheory {
+    /**
+     * [SOLUTION]: dp[n] depends on dp[n - 1] and dp[n - 2]. 
+     * dp[n] can't be true only if dp[n - 1] == dp[n - 2] == true.
+     * Use dp[] to avoid overlap.
+     */
+    public boolean firstWillWin(int n) {
+        if (n <= 0) return false;
+        boolean[] dp = new boolean[n + 1];
+        dp[1] = dp[2] = true;
+        for (int i = 3;i <= n;i++) {
+            dp[n] = !dp[n - 1] || !dp[n - 2];
+        }
+        return dp[n];
+    }
+    
+    /**
+     * dp[i]: the max number that can get in [i, end]. eg: dp[n] = 0, dp[n - 1] = values[n - 1]...
+     * when at i, 2 solutions:
+     *      1. take values[i], 
+     *          -> other person: 
+     *                  -> values[i+1]             -> then we can only choose from i+2 to end, max can get is dp[i+2]
+     *                  -> values[i+1]+values[i+2] -> can only choose from i+3 to end, max can get is dp[i+3]
+     *      2. take values[i]+values[i+1],
+     *          -> other person:
+     *                  -> values[i+2]             -> then can only choose from i+3 to end, max can get is dp[i+3]
+     *                  -> values[i+3]+values[i+4] -> then can only choose from i+5 to end, max can get is dp[i+5]
+     */
+    public boolean firstWillWin(int[] values) {
+        if (values == null || values.length == 0) return false;
+        int n = values.length;
+        if(n <= 2) return true;        
+        int[] dp = new int[n + 1];
+        dp[n] = 0;
+        dp[n - 1] = values[n - 1];
+        dp[n - 2] = values[n - 1] + values[n - 2];
+        dp[n - 3] = values[n - 3] + values[n - 2];
+        for (int i = n - 4;i >= 0;i--) {        // has to start from n - 4, since (i + 4) must <= n
+            // min1/min2 是自己有可能拿到的两种可能性，第一种是自己拿了values[i], 在对方的两种选择中，会给我们留下
+            // 能拿到的更小的一种选择
+            int min1 = values[i] + Math.min(dp[i + 2], dp[i + 3]);  
+            int min2 = (values[i] + values[i + 1]) + Math.min(dp[i + 3], dp[i + 4]);
+            dp[i] = Math.max(min1, min2);
+        }
+        int sum = 0;
+        for (int v:values) sum += v;
+        return dp[0] * 2 > sum;
+    }
 
+    /**375. Guess Number Higher or Lower II
+     * dp[i][j]: when searching in [i, j], the min cost needed to guarantee 
+     * finding the number.
+     */
+    public int getMoneyAmount(int n) {
+        return getMinCost(0, n-1, new int[n][n]);
+    }
+    private int getMinCost(int start, int end, int[][] dp) {
+        if (start >= end) return 0;
+        if (dp[start][end] != 0) return dp[start][end];
+        
+        // since we can choose from [start, end], then of course we will choose
+        // the number that indicates less cost/min cost
+        int minCost = Integer.MAX_VALUE;
+        for (int i = start;i <= end;i++) {  // iL the number we can guess
+            // use max, since we need to find a guarantee cost/a max cost
+            int cost = (i + 1) + Math.max(getMinCost(start, i-1, dp), 
+                                getMinCost(i+1, end, dp));
+            minCost = Math.min(cost, minCost);
+        }
+        dp[start][end] = minCost;
+        return dp[start][end];
+    }
 }
